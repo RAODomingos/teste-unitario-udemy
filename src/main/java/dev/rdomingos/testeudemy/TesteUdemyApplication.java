dd0c3ed1b83e2ec29f47381a2cbd1862ea621e0e
Romulo Domingos <romulo.domingos11@gmail.com>
2024-11-27 23:01:49 -0300

    Projeto Iniciado

---
+++ src/main/java/dev/rdomingos/testeudemy/TesteUdemyApplication.java
@@ -0,0 +1,13 @@
+package dev.rdomingos.testeudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/rdomingos/testeudemy/TesteUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rdomingos.testeudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
