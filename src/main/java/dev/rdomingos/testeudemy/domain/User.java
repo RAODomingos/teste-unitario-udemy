ea0e754a7049e617813e7fc6d0cb67d8de439afa
Romulo Domingos <romulo.domingos11@gmail.com>
2024-11-27 23:12:48 -0300

    Classe User

---
+++ src/main/java/dev/rdomingos/testeudemy/domain/User.java
@@ -0,0 +1,26 @@
+package dev.rdomingos.testeudemy.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "TB_USER")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
