a2df40e14a0fee677c0941f96d5325c8b1ccf0a6
Romulo Domingos <romulo.domingos11@gmail.com>
2024-11-27 23:40:13 -0300

    Classe UserResource - findById

--- src/main/java/dev/rdomingos/testeudemy/resource/UserResource.java
+++ src/main/java/dev/rdomingos/testeudemy/resource/UserResource.java
@@ -1,6 +1,8 @@
 package dev.rdomingos.testeudemy.resource;
 
 import dev.rdomingos.testeudemy.domain.User;
+import dev.rdomingos.testeudemy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private  UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok(new User(id, "Rafael Domingos", "Q2V0W@example.com", "123456"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
 
